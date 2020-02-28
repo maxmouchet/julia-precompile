06b5f5672af9e7e0b78fc0d085273762718f177d
Maxime Mouchet <max@maxmouchet.com>
2020-02-28 18:09:29 +0100

    Skip existing precompile files

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # julia-precompile
 
 ![precompile](https://github.com/maxmouchet/julia-precompile/workflows/precompile/badge.svg)
-
-**TODO:** Version in precompile file to avoid recompilation if no changes
--- precompile.jl
+++ precompile.jl
@@ -5,9 +5,15 @@ Pkg.instantiate()
 Pkg.precompile()
 
 for (name, version) in Pkg.installed()
+    output = "statements/$(name)-$(version).jl"
+    if isfile(output)
+        println("$output aready exists, skipping")
+        continue
+    end
+
     println("Running tests for $name $version")
     try
-        Pkg.test(name, julia_args=["--compile=all", "--trace-compile=statements/$(name)-$(version).jl"])
+        Pkg.test(name, julia_args=["--compile=all", "--trace-compile=$output"])
     catch e
         @warn e
     end
