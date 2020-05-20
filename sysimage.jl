67c4359dc062584b08cd8b3ca3d7e9bda635af32
Maxime Mouchet <max@maxmouchet.com>
2020-05-20 12:02:47 +0200

    Add sysimage.jl

---
+++ sysimage.jl
@@ -0,0 +1,17 @@
+using Glob
+using Pkg
+using PackageCompiler
+
+project = Pkg.TOML.parsefile("Project.toml")
+packages = Symbol.(keys(project["deps"]))
+@show packages
+
+# TODO: Remove old statements
+statements = glob("statements/*.jl")
+
+create_sysimage(
+    packages,
+    project=@__DIR__,
+    replace_default=true,
+    precompile_statements_file=statements
+)
